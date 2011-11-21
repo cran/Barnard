39ed8a9f5ef324cb4016677aa818f5b49761b764
Kamil Erguler <erguler.kamil@ucy.ac.cy>
2011-11-21 00:00:00 +0000

    version 1.2

--- R/barnardw.test.R
+++ R/barnardw.test.R
@@ -1,25 +1,26 @@
 `barnardw.test` <-
-function(n1,n2,n3,n4,one.sided=FALSE,dp=0.001) {
+function(n1,n2,n3,n4,dp=0.001) {
   ret=.C("BarnardW",
     as.integer(n1),
     as.integer(n2),
     as.integer(n3),
     as.integer(n4),
-    one.sided = as.integer(one.sided),
     dp = as.numeric(dp),
     nuisance.vector.x = as.double(vector("double",1.0+1.0/dp)),
-    nuisance.vector.y = as.double(vector("double",1.0+1.0/dp)),
-    wald.statistic = as.double(0.0))
+    nuisance.vector.y0 = as.double(vector("double",1.0+1.0/dp)),
+    nuisance.vector.y1 = as.double(vector("double",1.0+1.0/dp)),
+    wald.statistic = as.double(0.0),
+    nuisance.parameter = as.double(vector("double",2.0)),
+    p.value = as.double(vector("double",2.0)))
 
-  nuisance.matrix<-matrix(cbind(ret$nuisance.vector.x,ret$nuisance.vector.y),ncol=2)
-  rett<-nuisance.matrix[nuisance.matrix[,2]==max(nuisance.matrix[,2]),]
+  nuisance.matrix<-matrix(cbind(ret$nuisance.vector.x,ret$nuisance.vector.y0,ret$nuisance.vector.y1),ncol=3)
   
   return(list(nuisance.matrix = nuisance.matrix,
               dp = ret$dp,
               contingency.matrix = matrix(c(ret[[1]],ret[[2]],ret[[3]],ret[[4]]),ncol=2,byrow=TRUE),
-              alternative = c("Two Sided","One Sided")[as.integer(ret$one.sided==1)+1],
+              alternative = c("One Sided","Two Sided"),
               wald.statistic = ret$wald.statistic,
-              nuisance.parameter = rett[[1]],
-              p.value = rett[[2]]))
+              nuisance.parameter = ret$nuisance.parameter,
+              p.value = ret$p.value))
 }
 
--- src/Barnard.c
+++ src/Barnard.c
@@ -1,8 +1,7 @@
 #include "R.h"
 #include "Rmath.h"
 
-// Note: Follows statXact as a convention to exclude observation along the boundary of the Wald statistic
-void BarnardW(int *a, int *b, int *c, int *d, int *one_sided, double *dp, double *nuisance_vector_x, double *nuisance_vector_y, double *wald_statistic) {
+void BarnardW(int *a, int *b, int *c, int *d, double *dp, double *nuisance_vector_x, double *nuisance_vector_y0, double *nuisance_vector_y1, double *wald_statistic, double *nuisance_parameter, double *p_value) {
   int i,j;
   int c1  = (*a)+(*c);
   int c2  = (*b)+(*d);
@@ -12,47 +11,57 @@ void BarnardW(int *a, int *b, int *c, int *d, int *one_sided, double *dp, double
   (*wald_statistic) = (pxo<=0 || pxo>=1) ? 0 : (((double)((*b))/(double)(c2))-((double)((*a))/(double)(c1))) / sqrt(pxo*(1.0-pxo)*(irat));
 
   double ps  = 1.0+1.0/(*dp);
-  double *IJ = (double*)Calloc(2.0*(c1+1)*(c2+1),double);
+  double *IJ = (double*)Calloc(3.0*(c1+1)*(c2+1),double);
 
-  double txo = (*one_sided) ? (*wald_statistic) : fabs(*wald_statistic);
+  double txo = *wald_statistic;
   double tx;
   double px;
   int ccc=0;
-  if (*one_sided) {
-    if (txo<0) {
-      for (i=0; i<=c1; i++) for (j=0; j<=c2; j++) {
-	  px = (double)(i+j)/(double)(n);
-	  tx = (px<=0 || px>=1) ? 0 : (((double)j/(double)c2)-((double)i/(double)c1)) / sqrt(px*(1.0-px)*(irat));
-	  if (tx<txo || (i==(*a) && j==(*b))) {IJ[ccc++] = i; IJ[ccc++] = j;}
-	}
-    } else {
-      for (i=0; i<=c1; i++) for (j=0; j<=c2; j++) {
-	  px = (double)(i+j)/(double)(n);
-	  tx = (px<=0 || px>=1) ? 0 : (((double)j/(double)c2)-((double)i/(double)c1)) / sqrt(px*(1.0-px)*(irat));
-	  if (tx>txo || (i==(*a) && j==(*b))) {IJ[ccc++] = i; IJ[ccc++] = j;}
-	}
-    }
-  } else {
-    for (i=0; i<=c1; i++) for (j=0; j<=c2; j++) {
-	px = (double)(i+j)/(double)(n);
-	tx = (px<=0 || px>=1) ? 0 : (((double)j/(double)c2)-((double)i/(double)c1)) / sqrt(px*(1.0-px)*(irat));
-	if (fabs(tx)>txo || (i==(*a) && j==(*b)) || (c1==c2 && i==(*b) && j==(*a))) {IJ[ccc++] = i; IJ[ccc++] = j;}
+
+  for (i=0; i<=c1; i++) for (j=0; j<=c2; j++) {
+      px = (double)(i+j)/(double)(n);
+      tx = (px<=0 || px>=1) ? 0 : (((double)j/(double)c2)-((double)i/(double)c1)) / sqrt(px*(1.0-px)*(irat));
+      if (i==(*a) && j==(*b)) {
+	IJ[ccc++] = i; IJ[ccc++] = j; IJ[ccc++] = 1;
+      } else if (c1==c2 && i==(*b) && j==(*a)) {
+	IJ[ccc++] = i; IJ[ccc++] = j; IJ[ccc++] = 0;
+      } else if (txo<0) {
+	if (tx<txo) {IJ[ccc++] = i; IJ[ccc++] = j; IJ[ccc++] = 1;}
+	if (tx>-txo) {IJ[ccc++] = i; IJ[ccc++] = j; IJ[ccc++] = 0;}
+      } else {
+	if (tx>txo) {IJ[ccc++] = i; IJ[ccc++] = j; IJ[ccc++] = 1;}
+	if (tx<-txo) {IJ[ccc++] = i; IJ[ccc++] = j; IJ[ccc++] = 0;}
       }
-  }
+    }
 
   double n1  = lgamma(c1+1);
   double n2  = lgamma(c2+1);
-  double p;
+  double p, ad=0;
   int k, ii;
+
+  nuisance_parameter[0] = 0.0; nuisance_parameter[1] = 0.0;
+  p_value[0] = 0.0; p_value[1] = 0.0;
   for (k=0; k<ps; k++) {
     p = (double)(k)*(*dp);
     nuisance_vector_x[k] = p;
-    nuisance_vector_y[k] = 0;
+    nuisance_vector_y0[k] = 0;
+    nuisance_vector_y1[k] = 0;
 
-    for (ii=0; ii<ccc; ii+=2) {
+    for (ii=0; ii<ccc; ii+=3) {
       i = IJ[ii];
       j = IJ[ii+1];
-      nuisance_vector_y[k] += exp(n1+n2+(double)(i+j)*log(p)+(double)(n-i-j)*log(1.0-p)-(lgamma(i+1)+lgamma(j+1)+lgamma(c1-i+1)+lgamma(c2-j+1)));
+      ad = exp(n1+n2+(double)(i+j)*log(p)+(double)(n-i-j)*log(1.0-p)-(lgamma(i+1)+lgamma(j+1)+lgamma(c1-i+1)+lgamma(c2-j+1)));
+      if (IJ[ii+2]) nuisance_vector_y0[k] += ad;
+      nuisance_vector_y1[k] += ad;
+    }
+
+    if (nuisance_vector_y0[k] >= p_value[0]) {
+      nuisance_parameter[0] = nuisance_vector_x[k];
+      p_value[0] = nuisance_vector_y0[k];
+    }
+    if (nuisance_vector_y1[k] >= p_value[1]) {
+      nuisance_parameter[1] = nuisance_vector_x[k];
+      p_value[1] = nuisance_vector_y1[k];
     }
   }
 
